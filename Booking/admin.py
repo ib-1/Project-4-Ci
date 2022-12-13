69fc36f2071d6a7fe67539c26b1fe15cf957cfc3
ib-1 <ibrahimm.ali812@gmail.com>
2022-12-13 22:27:53 +0000

    created room model

--- Booking/admin.py
+++ Booking/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import room
 
 # Register your models here.
+admin.site.register(room)
+
--- Booking/models.py
+++ Booking/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+class room(models.Model):
+    room_type = (
+        ('SBD', 'SINGLE_BED'),
+        ('DBD', 'DOUBLE_BED'),
+        ('VIP', 'DELUXE_ROOM'),
+    
+    )
+    number = models.IntegerField()
+    catergory = models.CharField(max_length=3, choices=room_type)
+    beds = models.IntegerField()
+    capacity = models.IntegerField()
+
+    def __str__(self):
+        return f'{self.number}. {self.catergory} with {self.beds} beds for {self.capacity} people'
