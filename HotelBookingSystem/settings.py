4c1b43a0eaabc0c8b1aef52a378e9f37f09cf3af
ib-1 <ibrahimm.ali812@gmail.com>
2022-12-15 05:53:15 +0000

    fix to heroku deployment

--- HotelBookingSystem/settings.py
+++ HotelBookingSystem/settings.py
@@ -81,8 +81,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    "django.middleware.security.SecurityMiddleware",
-    "whitenoise.middleware.WhiteNoiseMiddleware",
 ]
 
 AUTHENTICATION_BACKENDS = [
