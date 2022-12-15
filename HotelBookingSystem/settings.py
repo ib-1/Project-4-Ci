08cd9127a40848a4e2cd4e6101939f1c594a95de
ib-1 <ibrahimm.ali812@gmail.com>
2022-12-15 05:51:45 +0000

    static file fix

--- HotelBookingSystem/settings.py
+++ HotelBookingSystem/settings.py
@@ -81,6 +81,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
 ]
 
 AUTHENTICATION_BACKENDS = [
